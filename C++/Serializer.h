9e2e9d5fd94acd5b2792e8899cb0be96610c6964
Clement-pirelli <clepirelli@gmail.com>
2020-04-19 00:45:41 +0200

    Removed unnecessary Serializer class
    fixed bug where serializer would assert the wrong way (<= instead of >=)
    
    Added getNext for arrays

--- C++/Serializer.h
+++ C++/Serializer.h
@@ -2,46 +2,48 @@
 #include <cstdint>
 #include <cassert>
 
-template<class T>
-class Serializer{
-public:
-	static T deserialize(uint8_t *data, size_t size)
-	{
-		assert(size <= sizeof(T));
-
-		return *(reinterpret_cast<T *>(data));
-	}
-
-	static uint8_t *serialize(const T &t) noexcept
-	{
-		return reinterpret_cast<uint8_t>(&t);
-	}
-private:
-	Serializer() = delete;
-	Serializer(const Serializer &other) = delete;
-};
-
 class StreamIn
 {
 public:
 
-	StreamIn(uint8_t *givenData, size_t givenMaxSize):data(givenData), maxSize(givenMaxSize){}
+	StreamIn(uint8_t *givenData, size_t givenMaxSize) :data(givenData), maxSize(givenMaxSize) {}
 
 	template<typename T>
 	T getNext() {
-		uint8_t *address = data + at;
+		size_t oldAt = at;
 		at += sizeof(T);
 
-		return Serializer<T>::deserialize(address,maxSize-at);
+		return deserialize<T>(data + oldAt, maxSize - oldAt);
+	}
+
+	template<typename T>
+	void getNext(T *dataToFill, size_t amount)
+	{
+		for (size_t i = 0; i < amount; i++)
+		{
+			size_t oldAt = at;
+			at += sizeof(T);
+
+			dataToFill[i] = deserialize<T>(data + oldAt, maxSize - oldAt);
+		}
 	}
 
 	template<typename T>
 	T peekNext() {
 		uint8_t *address = data + at;
-		return Serializer<T>::deserialize(address, maxSize - at);
+		return deserialize<T>(address, maxSize - at);
 	}
 
 private:
+
+	template<typename T>
+	static T deserialize(uint8_t *data, size_t size)
+	{
+		assert(size >= sizeof(T));
+
+		return *(reinterpret_cast<T *>(data));
+	}
+
 	uint8_t *data = nullptr;
 	size_t at = 0U;
 	size_t maxSize = 0U;
@@ -56,24 +58,22 @@ public:
 	~StreamOut() { delete[] data; }
 
 	template<typename T>
-	void setNext(const T &item) 
+	void setNext(const T &item)
 	{
 		uint8_t *address = data + at;
 		at += sizeof(T);
-		assert(at <= maxSize);
 		memcpy(address, &item, sizeof(T));
 	}
 
 	template<typename T>
-	void setNext(T *items, size_t amount) 
+	void setNext(T *items, size_t amount)
 	{
 		uint8_t *address = data + at;
-		at += sizeof(T)*amount;
-		assert(at <= maxSize);
-		memcpy(address, items, sizeof(T)*amount);
+		at += sizeof(T) * amount;
+		memcpy(address, items, sizeof(T) * amount);
 	}
 
-	uint8_t *getData() 
+	uint8_t *getData()
 	{
 		return data;
 	}
